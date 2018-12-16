5b102717be40ab58cdbfc85e06560bb69b15007d
tomi <tomel95@hotmail.com>
2018-12-16 16:02:13 -0300

    visuals

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
+import List from './components/list';
 
-const Container = styled.div`
+const MainContainer = styled.main`
   display: flex;
-  background: blue;
-  align-items: center;
+  background: #f5f5f5;
+  padding-top: 50px;
+  align-items: flex-start;
   justify-content: center;
   min-height: 100vh;
 `;
 
 const App = () => (
-  <Container className="App">
-    hola
-  </Container>
+  <MainContainer className="App">
+    <List />
+  </MainContainer>
 );
 
 export default App;
