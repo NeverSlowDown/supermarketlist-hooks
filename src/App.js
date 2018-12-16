95848e50a91cd9e5cc765b3f6e1ad6548180a58e
tomi <tomel95@hotmail.com>
2018-12-16 15:06:40 -0300

    setting configs

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from 'styled-components';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const Container = styled.div`
+  display: flex;
+  background: blue;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+`;
+
+const App = () => (
+  <Container className="App">
+    hola
+  </Container>
+);
 
 export default App;
